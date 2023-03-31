e5885d6adbf2cb650595cd3e2bbec39931498133
Saba Madadi <sabamadadi9@gmail.com>
2023-03-31 19:58:14 +0330

    Classes Updated!

--- src/main/java/Admin.java
+++ src/main/java/Admin.java
@@ -1,26 +1,26 @@
 import java.util.Scanner;
 
 public class Admin {
-    static Shop shop;
-    static String username;
-    static int password;
-    static String email;
-
-    public Admin(String input_username, int input_password, String input_email, Shop input_shop) {
-        username = input_username;
-        password = input_password;
-        email = input_email;
-        shop = input_shop;
-    }
-
-    public void EditProfile() {
-        Scanner cs = new Scanner(System.in);
-        System.out.println("Enter new password:");
-        String new_password = cs.nextLine();
-        int hash_password = shop.authenticator.Hash(new_password);
-        password = hash_password;
-        System.out.println("Enter new email address:");
-        email = cs.nextLine();
-        return;
-    }
+	static Shop shop;
+	static String username;
+	static int password;
+	static String email;
+	
+	public Admin(String input_username, int input_password, String input_email, Shop input_shop) {
+		username = input_username;
+		password = input_password;
+		email = input_email;
+		shop = input_shop;
+	}
+	
+	public void EditProfile() {
+		Scanner cs = new Scanner(System.in);
+		System.out.println("Enter new password:");
+		String new_password = cs.nextLine();
+		int hash_password = shop.authenticator.Hash(new_password);
+		password = hash_password;
+		System.out.println("Enter new email address:");
+		email = cs.nextLine();
+		return;
+	}
 }
